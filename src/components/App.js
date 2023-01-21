857e1d4d34b62299a4b7fb00ad45cb3e5b9ae52f
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-01-21 20:03:17 +0000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = {
+			isShown: true
+		}
+
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				<button
+					id="click"
+					type="button"
+					onClick={() => this.setState({ isShown: !this.state.isShown })}
+				>Show Para</button>
+				{this.state.isShown && <p id="para">{`Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`}</p>}
+			</div>
+		);
+	}
 }
 
 
